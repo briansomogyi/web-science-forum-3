57d06a6e33e201268ad19790db17e98310cc8b3f
briansomogyi <brian.somogyi@emanuel.ro>
2025-04-23 23:43:31 +0300

    Added "./src/components/CounterComponent.vue"

---
+++ vite-project/src/components/CounterComponent.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useCounter } from '../stores/counter'
+const counter = useCounter()
+function countUp() {
+    counter.count++
+}
+</script>
+<template>
+    <button @click="countUp">count is {{ counter.count }}</button>
+</template>
---
+++ vite-project/src/stores/counter.js
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+export const useCounter = defineStore('counter', {
+    state: () => {
+        return {
+            count: 0,
+        }
+    },
+})
