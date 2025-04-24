477d57b607606c5c6bf52c187acf669704f877d7
briansomogyi <brian.somogyi@emanuel.ro>
2025-04-24 09:37:30 +0300

    Integrated eslint with prettier

--- vite-project/.eslintrc.js
+++ vite-project/.eslintrc.js
@@ -1,10 +1,43 @@
 export default {
+    // Extend ESLint with Vue.js 3 strongly recommended rules
     extends: [
         'eslint:recommended',
-        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-strongly-recommended',
         'plugin:prettier/recommended',
     ],
-    rules: {
-        // Add any custom rules here
+
+    // Define the parser and environment
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
     },
+    env: {
+        browser: true,
+        node: true,
+    },
+
+    // Define general rules
+    rules: {
+        'no-console': 'warn',
+        'no-unused-vars': 'error',
+        'vue/html-self-closing': [
+            'error',
+            {
+                html: {
+                    void: 'always',
+                    normal: 'never',
+                },
+            },
+        ],
+    },
+
+    // Place the overrides block here
+    overrides: [
+        {
+            files: ['*.vue', '*.js'], // Apply to Vue and JS files
+            rules: {
+                'prettier/prettier': 'error', // Treat Prettier errors as ESLint errors
+            },
+        },
+    ],
 }
