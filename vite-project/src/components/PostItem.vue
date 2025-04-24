c95317ac6dbe1619367ca7f439fa0bf5d8755a48
briansomogyi <brian.somogyi@emanuel.ro>
2025-04-24 09:15:32 +0300

    Created Components

---
+++ vite-project/src/components/PostItem.vue
@@ -0,0 +1,11 @@
+<!-- PostItem.vue -->
+<template>
+    <div @click="$emit('select-post', post)">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.content }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps(['post'])
+</script>
---
+++ vite-project/src/components/PostList.vue
@@ -0,0 +1,18 @@
+<!-- PostList.vue -->
+<template>
+    <div>
+        <PostItem v-for="post in posts" :key="post.id" :post="post" @select-post="handleSelectPost" />
+    </div>
+</template>
+
+<script setup>
+import { usePostStore } from '../stores/main'
+import PostItem from './PostItem.vue'
+
+const store = usePostStore()
+const posts = store.posts
+
+const handleSelectPost = (post) => {
+    console.log(post)
+}
+</script>
